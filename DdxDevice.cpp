d409c03507d9cfd21d5b84b52c8d4a9e338f9b8c
Antonino Calderone <antonino.calderone@gmail.com>
2018-07-03 11:38:16 +0100

    Update DdxDevice.cpp

--- DdxDevice.cpp
+++ DdxDevice.cpp
@@ -1,22 +1,9 @@
+// This file is part of the WinRayCast Application (a 3D Engine Demo).
 // Copyright (C) 2005 - 2018
 // Antonino Calderone (antonino.calderone@gmail.com)
-//
-// This file is part of the WinRayCast Application (a 3D Engine Demo).
-// This program is free software; you can redistribute it and/or modify 
-// it under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 2, or (at your option)
-// any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License along
-// with this program; see the file COPYING.  If not, write to the Free
-// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-// USA.
-
+// All rights reserved.  
+// Licensed under the MIT License. 
+// See COPYING file in the project root for full license information.
 
 #include "DdxDevice.h"
 
