8007e57ecce126f5309e0d2532fca71374611b66
Antonino Calderone <antonino.calderone@gmail.com>
2018-07-03 11:42:46 +0100

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,3 +1,10 @@
+// This file is part of the WinRayCast Application (a 3D Engine Demo).
+// Copyright (C) 2005 - 2018
+// Antonino Calderone (antonino.calderone@gmail.com)
+// All rights reserved.  
+// Licensed under the MIT License. 
+// See COPYING file in the project root for full license information.
+
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
 // Used by WinRayCast.rc
