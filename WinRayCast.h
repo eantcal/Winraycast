cfbea37d473f63f642eb0dcaef55f9a5cb01ba81
Antonino Calderone <antonino.calderone@gmail.com>
2018-07-03 11:41:18 +0100

    Update WinRayCast.h

--- WinRayCast.h
+++ WinRayCast.h
@@ -1,3 +1,10 @@
+// This file is part of the WinRayCast Application (a 3D Engine Demo).
+// Copyright (C) 2005 - 2018
+// Antonino Calderone (antonino.calderone@gmail.com)
+// All rights reserved.  
+// Licensed under the MIT License. 
+// See COPYING file in the project root for full license information.
+
 
 #if !defined(AFX_WINRAYCAST_H__F2EDDB7A_B46A_442D_A720_D338F0DC1341__INCLUDED_)
 #define AFX_WINRAYCAST_H__F2EDDB7A_B46A_442D_A720_D338F0DC1341__INCLUDED_
